08380f4f66828cfdfd4da933bf9b168c1f932b41
Evrim Ustabaş <71992453+evriim@users.noreply.github.com>
2024-09-13 15:55:16 +0300

    Create majority_element169.py

---
+++ challenges/LeetCode/TopInterview150/majority_element169.py
@@ -0,0 +1,14 @@
+"""
+The problem asks to find the majority element in an array, i.e., 
+the element that appears more than half the time. A natural approach is 
+to count the frequency of each element and then return the one with the highest count.
+"""
+class Solution(object):
+    def majorityElement(self, nums):
+        k={}
+        for counter in range(len(nums)):
+            if nums[counter] in k:
+                k[nums[counter]]+=1
+            else:
+                k[nums[counter]]=1
+        return max(k, key=k.get)
