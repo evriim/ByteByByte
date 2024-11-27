6ed12d579bd444c9aed5ebf98dda5b39d72bb628
Evrim Ustabaş <71992453+evriim@users.noreply.github.com>
2024-11-27 15:38:09 +0300

    Create day01.py

---
+++ challenges/HackerRank/Days_Of_Code30/day01.py
@@ -0,0 +1,18 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+# Declare second integer, double, and String variables.
+i2 = 6
+d2 = 6.6
+s2 = "six"
+# Read and save an integer, double, and String to your variables.
+i2 = int(input())
+d2 = float(input())
+s2 = str(input())
+# Print the sum of both integer variables on a new line.
+print(i+i2)
+# Print the sum of the double variables on a new line.
+print(d+d2)
+# Concatenate and print the String variables on a new line
+print(s+s2)
+# The 's' variable above should be printed first.
